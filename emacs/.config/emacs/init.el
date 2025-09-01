0a3c5e5c3700b60cd22f2a9d1bbcad98a7a51c45
metalnwood <metalnwood@gmail.com>
2025-09-02 11:40:39 +1200

    update init

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -8,7 +8,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(custom-safe-themes
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
+     "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
+     default))
+ '(package-selected-packages
+   '(all-the-icons company consult corfu counsel-projectile doom-modeline
+		   doom-themes evil-collection expand-region general
+		   helpful inf-elixir ivy-rich lispyville lua-mode
+		   magit marginalia move-text multiple-cursors
+		   orderless paredit rainbow-delimiters sly vertico
+		   which-key yasnippet-snippets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,3 +28,4 @@
  )
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
